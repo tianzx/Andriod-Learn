68f9a007d3394ecb27aae542d34316ec77b78484
tianzx <436011080@qq.com>
2016-02-24 20:38:35 +0800

    Intent Activity Learn

--- app/src/main/java/net/tianzx/myapplication/MainActivity.java
+++ app/src/main/java/net/tianzx/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package net.tianzx.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 /**
  * 相当于一个html中的js controller web中的servlet struts2中的action
@@ -21,15 +23,16 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button btnToSecond = (Button) this.findViewById(R.id.btn_to_second);
+        btnToSecond.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                //
+                Intent in = new Intent();
+                in.setClass(MainActivity.this, Main2Activity.class);
+                MainActivity.this.startActivity(in);
             }
         });
-
         /** one is shown data part
          *1:get needed data
          *2:convert data
