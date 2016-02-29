682093ffecf6dfaed160bff22693321ab9911a7b
tianzx <436011080@qq.com>
2016-02-29 21:34:00 +0800

    "Learning "

--- app/src/main/java/net/tianzx/myapplication/ButtonActivity.java
+++ app/src/main/java/net/tianzx/myapplication/ButtonActivity.java
@@ -2,6 +2,9 @@ package net.tianzx.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class ButtonActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class ButtonActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button);
+        Button button = (Button) this.findViewById(R.id.btn);
+        button.setOnClickListener(new MyBtnListener());
+    }
+
+    class MyBtnListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(ButtonActivity.this, "点击了", Toast.LENGTH_LONG).show();
+        }
     }
 }
