6b6f7c8be94641a1547b15e2343e8651d4d51a78
tianzx <436011080@qq.com>
2016-03-07 21:29:13 +0800

    Learning Handler

--- app/src/main/java/net/tianzx/myapplication/ButtonActivity.java
+++ app/src/main/java/net/tianzx/myapplication/ButtonActivity.java
@@ -1,10 +1,12 @@
 package net.tianzx.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class ButtonActivity extends AppCompatActivity {
 
@@ -13,14 +15,41 @@ public class ButtonActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button);
         Button button = (Button) this.findViewById(R.id.btn);
-        button.setOnClickListener(new MyBtnListener());
+//        button.setOnClickListener(new MyBtnListener());
     }
 
-    class MyBtnListener implements View.OnClickListener {
-
+//    class MyBtnListener implements View.OnClickListener {
+//
+//        @Override
+//        public void onClick(View v) {
+//            Toast.makeText(ButtonActivity.this, "点击了", Toast.LENGTH_LONG).show();
+//        }
+//    }
+    private int num = 0;
+    Handler h = new Handler() {
         @Override
-        public void onClick(View v) {
-            Toast.makeText(ButtonActivity.this, "点击了", Toast.LENGTH_LONG).show();
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            num++;
+            Log.i("tianzx","now handle"+num);
         }
+    };
+
+    Runnable r = new Runnable() {
+        @Override
+        public void run() {
+            Message msg = h.obtainMessage();
+            h.sendMessage(msg);
+
+            h.postDelayed(r,1000L);
+        }
+    };
+
+    public void start(View v) {
+        h.post(r);
     }
+
+    public void stop(View v) {h.removeCallbacks(r);
+    }
+
 }
